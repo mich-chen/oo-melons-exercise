e43e91b31fef76a73288a60f432f1581a871d786
Michelle Chen <mich.chen.94@gmail.com>
2020-07-17 12:34:12 -0700

    made AbstractMelonOrder class, set attributes specific to Domestic and International, and accounted for country_code

--- melons.py
+++ melons.py
@@ -1,8 +1,10 @@
 """Classes for melon orders."""
 
+class AbstractMelonOrder():
+    """An abstract base class that other Melon Orders inherit from."""
 
-class DomesticMelonOrder():
-    """A melon order within the USA."""
+    tax = None
+    order_type = None
 
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
@@ -10,8 +12,6 @@ class DomesticMelonOrder():
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -27,31 +27,25 @@ class DomesticMelonOrder():
         self.shipped = True
 
 
-class InternationalMelonOrder():
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    tax = 0.08
+    order_type = 'domestic'
+
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
+
+    tax = 0.17
+    order_type = "international"
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
-
-        self.species = species
-        self.qty = qty
+        super().__init__(species, qty)
         self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
+    
 
     def get_country_code(self):
         """Return the country code."""
